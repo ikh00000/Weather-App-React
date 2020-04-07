eae3db4c59f1253abef654b8231dc65af4ce048f
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-08 02:12:07 +0300

    did bad things :)

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import './App.scss';
@@ -17,6 +18,29 @@ function App() {
       </div>
     </Provider>
 
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> parent of 32bb832... redux, axios, components
   );
 }
 
--- weather-app/src/components/WeatherSearchAndShow/keys.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = '115f06c9cb74251853d88e3df0fd0b36'; 
-export const API_CALL = (CITY_ID) => {
-  return `http://api.openweathermap.org/data/2.5/forecast?q=${CITY_ID}&APPID=${API_KEY}`
-}
-
