e6b184f4626796e5b1a91209d5f00605d32cda64
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-30 22:19:32 +0300

    build added

---
+++ build/precache-manifest.abebc64dfe320ba53efd1f3104e7914b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052274fa858d53c3331c8ad26f2142c7",
+    "url": "/Weather-App-React/index.html"
+  },
+  {
+    "revision": "95ab690ed8eccc63b574",
+    "url": "/Weather-App-React/static/css/main.f19b9f59.chunk.css"
+  },
+  {
+    "revision": "bd9d74f518842af8b489",
+    "url": "/Weather-App-React/static/js/2.1212b98a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-App-React/static/js/2.1212b98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ab690ed8eccc63b574",
+    "url": "/Weather-App-React/static/js/main.13288aa9.chunk.js"
+  },
+  {
+    "revision": "95b76ada80f01bd61ba5",
+    "url": "/Weather-App-React/static/js/runtime-main.52854d46.js"
+  },
+  {
+    "revision": "7c3643d152091a58b119ba6827ba3dfa",
+    "url": "/Weather-App-React/static/media/background-blur.7c3643d1.jpg"
+  }
+]);
